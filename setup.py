4c44879206265c116f35ce2b433cb5f5d6c65825
Anis Rahman <anisr@HP4CX34M7C.local>
2024-10-31 12:51:43 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CHMPredict",
+    version="0.1.0",
+    author="@nis",
+    license="GPLv3+",
+    packages=find_packages(include=["chmpredict"]),
+    install_requires=[
+        'pillow',
+        'rasterio',
+        'torchvision',
+        'scikit-learn',
+        'configargparse',
+    ],
+)
